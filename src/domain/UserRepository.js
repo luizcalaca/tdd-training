ee840b28a8cf28b9145100e686015d724107858b
Luiz Calaça <luizcalaca@gmail.com>
2022-08-10 16:48:55 -0300

    refactor: only persist

--- src/domain/UserRepository.js
+++ src/domain/UserRepository.js
@@ -6,24 +6,4 @@ module.exports = class {
     throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
   }
 
-  merge(domainUser) {
-    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
-  }
-
-  remove(userId) {
-    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
-  }
-
-  get(userId) {
-    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
-  }
-
-  getByEmail(email) {
-    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
-  }
-
-  find() {
-    throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
-  }
-
 };
